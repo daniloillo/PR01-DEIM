e39d0e944958188548a3f1057f8cd5927f0a5206
Daniel López <daniellopezblanco26@gmail.com>
2021-09-17 17:51:13 +0200

    Ejercicio 2a

--- PR01-DEIM/Assets/Scripts/Cubo.cs
+++ PR01-DEIM/Assets/Scripts/Cubo.cs
@@ -6,9 +6,7 @@ public class Cubo : MonoBehaviour
 {
 
     public int num1 = 1;
-    public float num2 = 2.3f;
-    public bool myBool = true;
-    public string saludo = "Buenos Dias Programador";
+  
 
 
 
@@ -18,8 +16,7 @@ public class Cubo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print(num1 + " " + saludo);
-        print("Mi numero favorito es el " + num2);
+        
         
 
 
@@ -32,7 +29,18 @@ public class Cubo : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {  
         
+        if (num1 <= 17){
+
+            print(num1);
+        
+        
+        
+        
+        
+        }
+
+        num1 = (num1 + 1);
     }
 }
