878f626a7f492b30c3a8f81c09510c91c0595654
Daniel López <daniellopezblanco26@gmail.com>
2021-09-17 17:27:53 +0200

    Ejercicio 1a

---
+++ PR01-DEIM/Assets/Scripts/Cubo.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubo : MonoBehaviour
+{
+
+    int num1 = 1;
+    float num2 = 2.3f;
+    bool myBool = true;
+    string saludo = "Buenos Dias Programador";
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(num1);
+        print(num2);
+        print("MyBool está en " + myBool);
+        print(saludo);
+
+
+
+
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
