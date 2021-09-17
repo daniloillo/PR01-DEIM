0b2c25abaee9f0fbaa29940699fc1ca2f1068e1c
Daniel López <daniellopezblanco26@gmail.com>
2021-09-17 19:06:18 +0200

    Ejercicio 2c (Contador Numeros Par/Impar)

--- PR01-DEIM/Assets/Scripts/Cubo.cs
+++ PR01-DEIM/Assets/Scripts/Cubo.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Cubo : MonoBehaviour
 {
 
-    public int n = 1;
+    public int n = 0;
   
 
 
@@ -16,9 +16,19 @@ public class Cubo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for(; n<= 10;n++)
+        for(; n<= 20;n++)
         {
-            print(n);
+            if (n % 2 == 0)
+            {
+                print(n + " es par");
+
+
+
+            }
+            else
+            {
+                print(n + " es impar");
+            }
       
          
 
